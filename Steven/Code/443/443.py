78c9cc8430792412bd0de9995b358686988800dd
steven06360161 <clps94038@gmail.com>
2024-08-14 22:58:37 +0800

    feature: 新增 leetcode 443

---
+++ Steven/Code/443/443.py
@@ -0,0 +1,21 @@
+class Solution:
+    def compress(self, chars: list[str]) -> int:
+        count_list = []
+        chars_sort = sorted(chars)
+        char_dict = {char:0 for char in chars_sort}
+
+        for chat in chars:
+            char_dict[chat] += 1
+        for key, value in char_dict.items():
+            if value == 1:
+                count_list.append(key)
+            else:
+                count_list.append(key)
+                inter_list = list(str(value))
+                for inter in inter_list:
+                    count_list.append(str(inter))
+
+        return count_list
+
+sol = Solution()
+print(sol.compress(['b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b','a','c']))
