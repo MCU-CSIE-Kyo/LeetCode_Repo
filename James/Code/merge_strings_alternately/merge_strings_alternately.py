cb7c018347a9aa0680cbd5b7b97f0e7d0e3d460f
jameslatcc <jameslatcc@gmail.com>
2024-08-29 11:11:39 +0800

    feat: move codes to corresponding folders
    put codes to corresponding folders for categorizing.

--- James/Code/merge_strings_alternately/merge_strings_alternately.py
+++ James/Code/merge_strings_alternately/merge_strings_alternately.py
@@ -1,18 +1,16 @@
 class Solution:
     def mergeAlternately(self, word1: str, word2: str) -> str:
-        lew1 = len(word1)
-        lew2 = len(word2)
+        length_word1 = len(word1)
+        length_word2 = len(word2)
+        minimum = min(length_word1, length_word2)
         s = ""
-        minimum = min(lew1, lew2)
         r = []
 
         for i in range(minimum):
             r.append(word1[i])
             r.append(word2[i])
-        
-        if lew1 > lew2:
+        if length_word1 > length_word2:
             r.append(word1[minimum:])
-        elif lew1 < lew2:
+        elif length_word1 < length_word2:
             r.append(word2[minimum:])
-        
         return s.join(r)
